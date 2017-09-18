30a7562ea24c0249c15a596141a4ce280d621a90
Qi Zhang <zhangqitydx@gmail.com>
2017-09-18 18:07:07 -0500

    Fix Inner Loop; Use space instead of tab;

--- site.js
+++ site.js
@@ -1,61 +1,45 @@
-var createTable = function(){
-	'use strict';
-
-	var  users = [{
-			"name":"Qi Zhang",
-			"gitname": "QiZhang79",
-		    "id": "001",
-		    "location": "Chicago",
-		    "social_media":"facebook"
-		},{
-			"name":"JunFeng Wang",
-			"gitname": "jwang206",
-		    "id": "002",
-		    "location": "Chicago",
-		    "social_media":"instagram"
-		},{
-			"name":"Sara Lopez",
-			"gitname": "slopez12",
-		    "id": "003",
-		    "location": "Chicago",
-		    "social_media":"instagram"
-		},{
-			"name":"Meiqi Zhao",
-			"gitname": "mzhao22",
-		    "id": "004",
-		    "location": "Chicago",
-		    "social_media":"facebook"
-		}
-	];
+var  users = [{
+		"name":"Qi Zhang",
+		"id": "001",
+		"location": "Chicago",
+		"social_media":"facebook",
+		"gitname": "QiZhang79",
+	},{
+		"name":"JunFeng Wang",
+		"id": "002",
+		"location": "Chicago",
+		"social_media":"instagram",
+		"gitname": "jwang206"
+	},{
+		"name":"Sara Lopez",
+		"id": "003",
+		"location": "Chicago",
+		"social_media":"instagram",
+		"gitname": "slopez12"
+	},{
+		"name":"Meiqi Zhao",
+		"id": "004",
+		"location": "Chicago",
+		"social_media":"facebook",
+		"gitname": "mzhao22"
+	}
+];
 
 
+$('#button').on("click",function(e){
+	var container = ["name", "id", "loaction", "social_media"];
+	var tbody = $("#user_table tbody");
 
-	$('#result').hide();
-	$('#button').on("click",function(e){
-		$('#result').show();
-		var container = ["name", "gitname", "id", "loaction"];
+	for(var i = 0; i < users.length; i++){
+		var row = '<tr>';
 
-		for(var i = 0; i < users.length; i++){
-			var $row = '<tr>';
-			var tbody = $("#user_table");
-
-			// for(var j = 0; j < 4; j++ ){
-			// 	$row += "<td>" + users[i]["name"] + "</td>";
-
-			// }
-			$row += "<td>" + users[i]["name"] + "</td>";
-			$row += '<td><a href="https://github.com/'
-  				+ users[i]["gitname"] + '">' + users[i]["gitname"] + '</a></td>'
-			$row += "<td>" + users[i]["id"] + "</td>";
-			$row += "<td>" + users[i]["location"] + "</td>";
-			$row += "<td>" + users[i]["social_media"] + "</td>";
-
-
-			$row += "</tr>";
-			tbody.append($row);
-		}
-
-	});
-};
-
-$(document).ready(createTable);
+		for(var j=0; j < container.length; j++) {
+			var record = container[j]
+			row += "<td>" + users[i][record] + "</td>";
+	  }
+		row += '<td><a href="https://github.com/'
+				+ users[i]["gitname"] + '">' + users[i]["gitname"] + '</a></td>'
+		row += "</tr>";
+		tbody.append(row);
+	}
+});
