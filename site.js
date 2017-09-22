8f8a1644c83f9d3a8bef6f35f93d9edd5bd0c771
Qi Zhang <zhangqitydx@gmail.com>
2017-09-21 21:13:58 -0500

    New: Get current time; Insert new student infos; Get images from Github Api

--- site.js
+++ site.js
@@ -4,29 +4,38 @@ var  users = [{
 		"location": "Chicago",
 		"social_media":"facebook",
 		"gitname": "QiZhang79",
+		"avatar_url": "https://avatars2.githubusercontent.com/u/21212693?v=4"
 	},{
 		"name":"JunFeng Wang",
 		"id": "002",
 		"location": "Chicago",
 		"social_media":"instagram",
-		"gitname": "jwang206"
+		"gitname": "jwang206",
+		"avatar_url": "https://avatars1.githubusercontent.com/u/16583879?v=4"
 	},{
 		"name":"Sara Lopez",
 		"id": "003",
 		"location": "Chicago",
 		"social_media":"instagram",
-		"gitname": "slopez12"
+		"gitname": "slopez12",
+		"avatar_url": "https://avatars1.githubusercontent.com/u/25110727?v=4"
 	},{
 		"name":"Meiqi Zhao",
 		"id": "004",
 		"location": "Chicago",
 		"social_media":"facebook",
-		"gitname": "mzhao22"
+		"gitname": "mzhao22",
+		"avatar_url": "https://avatars2.githubusercontent.com/u/21227715?v=4"
 	}
 ];
 
 
-$('#button').on("click",function(e){
+$("#datetime").append("It is " + new Date($.now()));
+
+
+var main = function(){
+	"use strict";
+
 	var container = ["name", "id", "location", "social_media"];
 	var tbody = $("#user_table tbody");
 
@@ -41,5 +50,48 @@ $('#button').on("click",function(e){
 				+ users[i]["gitname"] + '">' + users[i]["gitname"] + '</a></td>'
 		row += "</tr>";
 		tbody.append(row);
+
+		$("#pictures").append('<img src="' + users[i]['avatar_url'] + '" />')
 	}
-});
+
+
+
+	$("#user_form").on("submit", function(e) {
+	  var name = $("#name").val();
+	  var studentid = $("#studentid").val();
+	  var location = $("#location").val();
+	  var social_media = $("#social").val();
+	  var gitname = $("#gitname").val();
+	  var new_row = " ";
+
+	  if(name === "" && studentid === "" && location === "" && social_media === "" && gitname === ""){
+	  	alert("Please insert info : )");
+	    e.preventDefault();
+	  } else {
+	    new_row = "<tr><td>" + name + "</td><td>" + studentid 
+	            + "</td><td>" + location + "</td><td>" + social_media 
+	            + '</td><td><a href="https://github.com/' + gitname + '">'
+	            + gitname + '</a></td></tr>';
+	    tbody.append(new_row);
+
+	    $("#name").val("");
+	    $("#studentid").val("");
+	    $("#location").val("");
+	    $("#social").val("");
+	    $("#gitname").val("");
+	    e.preventDefault();
+	  }
+
+	});
+
+	$("#user_form").on("reset", function(e){
+	  $("#name").val("");
+	  $("#studentid").val("");
+	  $("#location").val("");
+	  $("#social").val("");
+	  $("#gitname").val("");
+	});
+};
+$(document).ready(main);
+
+
